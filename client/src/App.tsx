c9a880226f99d5f89848940fa8c04a40d3d8dfd2
vijay-dev07 <uk.kaithwas771@gmail.com>
2026-08-19 22:45:54 +0530

    setup  postgress database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,15 @@ import OrderTracking from "./pages/OrderTracking"
 import MyOrders from "./pages/MyOrders"
 import Addresses from "./pages/Addresses"
 import ProtectedRoute from "./components/ProtectedRoute"
+import AdminLayout from "./pages/admin/AdminLayout"
+import AdminDashboard from "./pages/admin/AdminDashboard"
+import AdminProducts from "./pages/admin/AdminProducts"
+import AdminProductForm from "./pages/admin/AdminProductForm"
+import AdminOrders from "./pages/admin/AdminOrders"
+import AdminDeliveryPartners from "./pages/admin/AdminDeliveryPartners"
+import DeliveryLogin from "./pages/delivery/DeliveryLogin"
+import DeliveryLayout from "./pages/delivery/DeliveryLayout"
+import DeliveryDashboard from "./pages/delivery/DeliveryDashboard"
 
 const App = () => {
   return (
@@ -37,6 +46,21 @@ const App = () => {
             <Route path="orders/:id" element={<OrderTracking/>}/>
             <Route path="addresses" element={<Addresses/>}/>
         </Route>
+      </Route>
+      {/* Admin pages */}
+      <Route path="/admin" element={<AdminLayout/>}>
+        <Route index element={<AdminDashboard/>}/>
+        <Route path="products" element={<AdminProducts/>}/>
+        <Route path="products/new" element={<AdminProductForm/>}/>
+        <Route path="products/:id/edit" element={<AdminProductForm/>}/>
+        <Route path="orders" element={<AdminOrders/>}/>
+        <Route path="delivery-partners" element={<AdminDeliveryPartners/>}/>
+      </Route>
+
+      {/* Delivery partner pages */}
+      <Route path="/delivery/login" element={<DeliveryLogin/>}/>
+      <Route path="/delivery" element={<DeliveryLayout/>}>
+        <Route index element={<DeliveryDashboard/>}/>
       </Route>
 
 
