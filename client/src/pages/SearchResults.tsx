b920248f10ac52a9d3e308b4cccd95f0e4510840
vijay-dev07 <uk.kaithwas771@gmail.com>
2026-08-19 21:24:39 +0530

    implemented search functionality

--- client/src/pages/SearchResults.tsx
+++ client/src/pages/SearchResults.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react"
 import type { Product } from "../types"
 import { Link, useSearchParams } from "react-router-dom"
 import { dummyProducts } from "../assets/assets"
-import { Home } from "lucide-react"
+import { Home, Search } from "lucide-react"
+import Loading from "../components/Loading"
+import ProductCard from "../components/ProductCard"
 
 const SearchResults = () => {
   const [products , setProducts] = useState<Product[]>([])
@@ -31,8 +33,28 @@ const SearchResults = () => {
 
           {/* Header */}
           <div className="mb-8">
-            <h1>Results for "{query}"</h1>
+            <h1 className="text-2xl font-semibold text-app-green mb-1">Results for "{query}"</h1>
+            <p className="text-sm text-app-text-light">{loading ? "Searching..." : `${products.length} items found`}</p>
           </div>
+
+          {/* Results */}
+          {loading ? (
+            <Loading/>
+
+          ): products.length === 0 ? (
+            <div className="text-center py-20">
+              <Search className="size-16 text-app-border mx-auto mb-4" />
+              <h2 className="text-xl font-semibold text-app-green mb-2">No results found</h2>
+              <p className="text-sm text-app-text-light mb-6 max-w-md mx-auto">we couldn't find any products matching "{query}". try a different search term.</p>
+              <Link to='/products' className="inline-flex px-5 py-2.5 bg-app-green text-white text-sm font-medium rounded-lg">Browse All Products</Link>
+            </div>
+          ) : (
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4"> 
+            {products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+            </div>
+          )}
       </div>
     </div>
   )
