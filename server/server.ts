48c457be917ed99a155c511966fec4b5b59cf5b8
vijay-dev07 <uk.kaithwas771@gmail.com>
2026-08-20 19:08:50 +0530

    created auth and product controller

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -13,6 +14,15 @@ const port = process.env.PORT || 5000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use('/api/auth' , authRouter)
+
+// error handling 
+app.use((error: any, req: Request , res:Response , next:NextFunction)=> {
+    console.error(error)
+    res.status(500).json({message:error.message})
+
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
