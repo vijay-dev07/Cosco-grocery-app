882558a12b9e1367eef61da0db08d3c0a134031d
vijay-dev07 <uk.kaithwas771@gmail.com>
2026-09-01 22:28:03 +0530

    completed order controller

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,10 @@ import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 
 const app = express();
 
@@ -20,6 +24,9 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/auth' , authRouter)
 app.use('/api/products' , productRouter)
 app.use('/api/upload' , uploadRouter)
+app.use('/api/orders' , orderRouter)
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // error handling 
 app.use((error: any, req: Request , res:Response , next:NextFunction)=> {
